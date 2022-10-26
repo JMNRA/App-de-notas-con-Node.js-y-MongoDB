dccd253fff07132de2d993cda7ea80ff19e2fa8f
JMNRA <jmedinan@unsa.edu.pe>
2022-10-25 19:49:19 -0500

    Configuraciones basicas

--- src/index.js
+++ src/index.js
@@ -1,2 +1,40 @@
 const express = require('express');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
+const session = require('express-session');
+// Initializations
 const app = express();
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine(
+  '.hbs',
+  exphbs({
+    defaultLayout: 'main.hbs',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+  })
+);
+app.set('views engine', '.hbs');
+// Midleware
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride('_method'));
+app.use(
+  session({
+    secret: 'mysecretapp ',
+    resave: true,
+    saveUninitialize: true,
+  })
+);
+
+// Global variables
+
+// Routes
+
+// Static files
+
+// Server settings
+app.listen(app.get('port'), () => {});
